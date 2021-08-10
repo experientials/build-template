77120f1955af21055f53266fe5c704ce614d0d13
Alex Lyr <alex.lyrakis@u-blox.com>
2021-08-10 11:54:02 +0300

    Arm64 try#6

--- Brain/Subcognition/C++/CMake/build/run_CMake_and_make.sh
+++ Brain/Subcognition/C++/CMake/build/run_CMake_and_make.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
 rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile
 cmake ..
+if [ $? -eq 0 ]; then
+   printf '########################\n\u2714\u2714\u2714 OK \u2714 \u2714 \u2714\n########################\n'
+else
+   printf '########################\n\u274c\u274c\u274c FAIL \u274c \u274c \u274c\n########################\n'
+   exit -1
+fi
 make
+if [ $? -eq 0 ]; then
+   printf '########################\n\u2714\u2714\u2714 OK \u2714 \u2714 \u2714\n########################\n'
+else
+   printf '########################\n\u274c\u274c\u274c FAIL \u274c \u274c \u274c\n########################\n'
+   exit -1
+fi
 ./hello-world-openCV
+if [ $? -eq 0 ]; then
+   printf '########################\n\u2714\u2714\u2714 OK \u2714 \u2714 \u2714\n########################\n'
+else
+   printf '########################\n\u274c\u274c\u274c FAIL \u274c \u274c \u274c\n########################\n'
+   exit -1
+fi
